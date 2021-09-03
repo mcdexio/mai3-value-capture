1d362b8fd459de6fff207be531036877de992fab
yangzhao <yangzhao@mcarlo.com>
2021-09-03 19:58:45 +0800

    update

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@
 const ENV = {
     ARBITRUM_RPC_ENDPOINT: "https://arb1.arbitrum.io/rpc",
     VALUE_CAPTURE_ADMIN_KEY: process.env["VALUE_CAPTURE_ADMIN_KEY"],
-    SLACK_BOT_API: "https://hooks.slack.com/services/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",
+    SLACK_BOT_API: "https://hooks.slack.com/services/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",
 }
 
 const ADDRESS = {
